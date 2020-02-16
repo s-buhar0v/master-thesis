3cf9e3d30636146ef7fb8e5fdfbdcb290f0d6cf9
Seregy Bukharov <s.buharov@dodopizza.com>
2020-02-16 18:59:14 +0300

    Put metrics exporter into dodcker

--- run-local.sh
+++ run-local.sh
@@ -1,2 +1,4 @@
-docker build -t sbukhar0v/master-thesis-init:latest .
+docker build -t sbukhar0v/master-thesis-init:latest -f init.Dockerfile .
+docker build -t sbukhar0v/master-thesis-metricsexporter:latest -f metricsexporter.Dockerfile .
+
 docker-compose up

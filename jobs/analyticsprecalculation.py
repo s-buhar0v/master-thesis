71edd5c67e67bc2afa83d35a667c4a717463ff81
Seregy Bukharov <s.buharov@dodopizza.com>
2020-04-13 22:10:05 +0300

    Refactor, add some analytics

--- jobs/analyticsprecalculation.py
+++ jobs/analyticsprecalculation.py
@@ -1,5 +1,6 @@
 import os
 import pymongo
+import itertools
 
 from datetime import datetime
 
@@ -7,6 +8,8 @@ client = pymongo.MongoClient(os.environ['MONGO_DB_CONNECTION_STRING'])
 db = client.masterthesis
 
 CITIES_TOP_COUNT = 5
+LANGUAGES_TOP_COUNT = 3
+
 CURRENT_YEAR = datetime.utcnow().year
 AGE_RANGES = [
     {
@@ -45,18 +48,66 @@ def _calculate_percentage(total, part, ndigits=1):
     return round(number=part / total * 100, ndigits=ndigits)
 
 
-def main():
-    age_distribution = []
+def _calculate_language_distribution():
+    language_distribution = []
+
+    users_with_filled_personal_data = list(
+        db.users.find({'personal': {'$ne': None}}, {'personal': 1})
+    )
+
+    print(users_with_filled_personal_data[0])
+    users_with_filled_languages = list(
+        filter(lambda u: 'langs' in u['personal'], users_with_filled_personal_data)
+    )
+    languages = set(
+        itertools.chain.from_iterable(
+            map(lambda u: u['personal']['langs'], users_with_filled_languages)
+        )
+    )
+
+    for language in languages:
+        count = len(list(filter(lambda u: language in u['personal']['langs'], users_with_filled_languages)))
+        language_distribution.append({
+            'lang': language,
+            'count': count,
+        })
+
+    language_distribution.sort(key=lambda l: l['count'])
+    language_distribution_items_count = len(language_distribution)
+
+    return {
+        'top': language_distribution[
+               language_distribution_items_count - LANGUAGES_TOP_COUNT: language_distribution_items_count],
+        'others': language_distribution[0: language_distribution_items_count - LANGUAGES_TOP_COUNT],
+    }
+
+
+def _calculate_cities_distribution():
     cities_distribution = []
 
-    total_users_count = db.users.count_documents({})
-    females = db.users.count_documents({'sex': 1})
-    males = db.users.count_documents({'sex': 2})
-    has_higher_education = db.users.count_documents({'universities': {'$ne': []}})
-    users_with_full_bdates = list(db.users.find({'bdate': {'$regex': '\d{1,2}.\d{1,2}.\d{4}'}}))
+    users_with_filled_city = list(db.users.find({'city': {'$ne': None}}, {'city': 1}))
+    cities = set(map(lambda c: c['city']['title'], users_with_filled_city))
 
-    users_with_city = list(db.users.find({'city': {'$ne': None}}, {'city': 1}))
-    cities = set(map(lambda c: c['city']['title'], users_with_city))
+    for city in cities:
+        count = len(list(filter(lambda u: u['city']['title'] == city, users_with_filled_city)))
+        cities_distribution.append({
+            'city': city,
+            'count': count,
+        })
+
+    cities_distribution.sort(key=lambda c: c['count'])
+    cities_distribution_items_count = len(cities_distribution)
+
+    return {
+        'top': cities_distribution[
+               cities_distribution_items_count - CITIES_TOP_COUNT: cities_distribution_items_count],
+        'others': cities_distribution[0: cities_distribution_items_count - CITIES_TOP_COUNT],
+    }
+
+
+def _calculate_age_distribution():
+    age_distribution = []
+    users_with_full_bdates = list(db.users.find({'bdate': {'$regex': '\d{1,2}.\d{1,2}.\d{4}'}}))
 
     for user_with_full_bday in users_with_full_bdates:
         user_with_full_bday['age'] = CURRENT_YEAR - int(user_with_full_bday['bdate'].split('.')[2])
@@ -74,40 +125,61 @@ def main():
             )
         })
 
-    for city in cities:
-        count = len(list(filter(lambda u: u['city']['title'] == city, users_with_city)))
-        cities_distribution.append({
-            'city': city,
-            'percentage': _calculate_percentage(total=len(users_with_city), part=count)
-        })
-
-    cities_distribution.sort(key=lambda c: c['percentage']),
-    cities_distribution_items_count = len(cities_distribution)
-    age_distribution.sort(key=lambda a: a['count']),
     age_distribution_items_count = len(age_distribution)
+    age_distribution.sort(key=lambda a: a['count']),
+
+    return {
+        'top': age_distribution[age_distribution_items_count - 1],
+        'others': age_distribution[0:age_distribution_items_count - 1],
+    }
+
+
+def _calculate_gender_distribution():
+    total_users_count = db.users.count_documents({})
+    females = db.users.count_documents({'sex': 1})
+    males = db.users.count_documents({'sex': 2})
+
+    return {
+        'males': males,
+        'females': females,
+        'total': total_users_count
+    }
+
+
+def _calculate_higher_education_percentage(total_users_count):
+    users_with_filled_universities = db.users.count_documents({'universities': {'$ne': []}})
+
+    return _calculate_percentage(total=total_users_count, part=users_with_filled_universities)
+
+
+def _calculate_relation_percentage(total_users_count):
+    # see https://vk.com/dev/objects/user_2
+    users_in_relation = db.users.count_documents({'relation': {'$in': [2, 3, 4, 7, 8]}})
+
+    return _calculate_percentage(total=total_users_count, part=users_in_relation)
+
+
+def main():
+    total_users_count = db.users.count_documents({})
 
     db.analytics.update_one(
         {
-            'group': group_name
+            'group': group_name,
         },
         {
             '$set': {
-                'last_update': datetime.utcnow().isoformat(),
-                'males_percentage': _calculate_percentage(total=total_users_count, part=males),
-                'females_percentage': _calculate_percentage(total=total_users_count, part=females),
-                'has_higher_education_percentage': _calculate_percentage(
-                    total=total_users_count,
-                    part=has_higher_education
+                'gender_distribution': _calculate_gender_distribution(),
+                'higher_education_percentage': _calculate_higher_education_percentage(
+                    total_users_count=total_users_count
                 ),
-                'cities_distribution': {
-                    'top': cities_distribution[cities_distribution_items_count - CITIES_TOP_COUNT: cities_distribution_items_count],
-                    'others': cities_distribution[0:cities_distribution_items_count - CITIES_TOP_COUNT],
-                },
-                'age_distribution': {
-                    'top': age_distribution[age_distribution_items_count - 1],
-                    'others': age_distribution[age_distribution_items_count - 1],
-                },
-                'group': 'w220.club'
+                'in_relation': _calculate_relation_percentage(
+                    total_users_count=total_users_count
+                ),
+                'cities_distribution': _calculate_cities_distribution(),
+                'age_distribution': _calculate_age_distribution(),
+                'lang_distribution': _calculate_language_distribution(),
+                'last_update': datetime.utcnow().isoformat(),
+                'group': group_name
             }
         },
         upsert=True

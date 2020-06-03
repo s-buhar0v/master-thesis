fd582d0cc6236f9fa38e05123ab3948988dfaf24
Seregy Bukharov <s.buharov@dodopizza.com>
2020-06-03 10:23:27 +0300

    Individual vk tokens

--- cron.Dockerfile
+++ cron.Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.7-slim
 
 ARG PYTHON_SCRIPT_NAME
+ARG VK_API_ACCESS_TOKEN
 
 ENV PYTHONPATH=$PYTHONPATH:$(pwd)
 
@@ -12,7 +13,7 @@ WORKDIR /app
 
 RUN pip3 install -r requirements.txt
 
-ENV VK_API_ACCESS_TOKEN="ab8c9b34ab8c9b34ab8c9b3465abfec6a6aab8cab8c9b34f55ab94d61c856e04bf13408"
+ENV VK_API_ACCESS_TOKEN=${VK_API_ACCESS_TOKEN}
 ENV MONGO_DB_CONNECTION_STRING="mongodb+srv://developer:38ea0b9d@master-thesis-kwsx7.azure.mongodb.net/masterthesis?retryWrites=true&w=majority"
 ENV PYTHON_SCRIPT_NAME=$PYTHON_SCRIPT_NAME
 
--- deploy/1-build.sh
+++ deploy/1-build.sh
@@ -16,6 +16,7 @@ function rebuild_image() {
 function rebuild_cron_image() {
     local image_name=$1
     local python_script_name=$2
+    local vk_api_access_token=$3
     containers=$(docker ps -a -q --filter ancestor=${image_name} --format="{{.ID}}")
 
     if [ ! -z $containers ]; then
@@ -26,6 +27,7 @@ function rebuild_cron_image() {
     docker build -t ${image_name} \
         -f "${PROJECT_PATH}/cron.Dockerfile" \
         --build-arg PYTHON_SCRIPT_NAME=${python_script_name} \
+        --build-arg VK_API_ACCESS_TOKEN=${vk_api_access_token} \
         ${PROJECT_PATH}
 }
 
@@ -52,8 +54,8 @@ rebuild_image ${INIT_IMAGE} ${PROJECT_PATH}/init.Dockerfile
 rebuild_image ${EXPORTER_IMAGE} ${PROJECT_PATH}/metricsexporter.Dockerfile
 rebuild_image ${ANALYTICSAPI_IMAGE} ${PROJECT_PATH}/analyticsapi.Dockerfile
 
-rebuild_cron_image ${METRICSPRECALCULATION_IMAGE} ${METRICSPRECALCULATION_APP_NAME}
-rebuild_cron_image ${ANALYTICSPRECALCULATION_IMAGE} ${ANALYTICSPRECALCULATION_APP_NAME}
-rebuild_cron_image ${POSTCOLLECTOR_IMAGE} ${POSTCOLLECTOR_APP_NAME}
-rebuild_cron_image ${POSTACTUALIZER_IMAGE} ${POSTACTUALIZER_APP_NAME}
-rebuild_cron_image ${USEDATACOLLECTOR_IMAGE} ${USEDATACOLLECTOR_APP_NAME}
+rebuild_cron_image ${METRICSPRECALCULATION_IMAGE} ${METRICSPRECALCULATION_APP_NAME} "1f2430701f2430701f243070871f5652c611f241f24307041f37f2d21473296dd58a7e8"
+rebuild_cron_image ${ANALYTICSPRECALCULATION_IMAGE} ${ANALYTICSPRECALCULATION_APP_NAME} "1f2430701f2430701f243070871f5652c611f241f24307041f37f2d21473296dd58a7e8"
+rebuild_cron_image ${POSTCOLLECTOR_IMAGE} ${POSTCOLLECTOR_APP_NAME} "1f2430701f2430701f243070871f5652c611f241f24307041f37f2d21473296dd58a7e8"
+rebuild_cron_image ${POSTACTUALIZER_IMAGE} ${POSTACTUALIZER_APP_NAME} "1486d6da1486d6da1486d6dad714f4b469114861486d6da4a5199c5e2dca4a277be184f"
+rebuild_cron_image ${USEDATACOLLECTOR_IMAGE} ${USEDATACOLLECTOR_APP_NAME} "1f2430701f2430701f243070871f5652c611f241f24307041f37f2d21473296dd58a7e8"
